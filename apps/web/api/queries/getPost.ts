b94cf8add089294682e4c019fce799b5727293f5
Joseph Abbey <joseph@abbey1.org.uk>
2022-07-26 12:43:10 +0100

    query examples 3 & 4

---
+++ apps/web/api/queries/getPost.ts
@@ -0,0 +1,26 @@
+import { t } from '@decago/object-definition';
+import db from '../../db/generated';
+
+export const getPostInput = new t.Model('getPostInput', {
+    id: t.int(),
+});
+
+export const getPostOutput = new t.Model('getPostOutput', {
+    id: t.int(),
+    title: t.string(),
+    content: t.string(),
+    createdAt: t.date(),
+    updatedAt: t.date(),
+});
+
+export default async function getUser(
+    input: t.infer<typeof getPostInput>
+): Promise<t.infer<typeof getPostOutput>> {
+    return (
+        await db.Posts({
+            where: {
+                id: input.id,
+            },
+        })
+    )[0];
+}
--- apps/web/api/queries/getPosts.ts
+++ apps/web/api/queries/getPosts.ts
@@ -1,14 +1,13 @@
 import { t } from '@decago/object-definition';
 import db from '../../db/generated';
 
-export const getUserPostsInput = new t.Model('getUserPostsInput', {
-    id: t.int(),
+export const getPostsInput = new t.Model('getPostsInput', {
     take: t.int(),
     skip: t.int(),
 });
 
-export const getUserPostsOutput = t.listOf(
-    new t.Model('getUserPostsOutputItem', {
+export const getPostsOutput = t.listOf(
+    new t.Model('getPostsOutputItem', {
         id: t.int(),
         title: t.string(),
         short_content: t.string(),
@@ -16,17 +15,11 @@ export const getUserPostsOutput = t.listOf(
     })
 );
 
-export default async function getUserPosts(
-    input: t.infer<typeof getUserPostsInput>
-): Promise<t.infer<typeof getUserPostsOutput>> {
-    return (
-        await db.Users({
-            where: {
-                id: input.id,
-            },
-        })
-    )[0]
-        .posts({
+export default async function getPosts(
+    input: t.infer<typeof getPostsInput>
+): Promise<t.infer<typeof getPostsOutput>> {
+    return db
+        .Posts({
             take: input.take,
             skip: input.skip,
         })
